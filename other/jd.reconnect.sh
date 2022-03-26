afcda30fc937a5b3025c5c7eb56b32ecfd4fda3b
ph202107 <85448312+ph202107@users.noreply.github.com>
2022-03-26 09:57:15 -0600

    Update jd.reconnect.sh
    Add option to show the server load

--- other/jd.reconnect.sh
+++ other/jd.reconnect.sh
@@ -27,9 +27,8 @@
 # (Use the absolute path)
 jdlist="/home/$USER/Downloads/jd.nordservers.txt"
 #
-# The initial list will be created based on your current location.
-# Specify alternate cities to use when the server list is emptied.
-# These will be used in rotation as subsequent server lists are emptied.
+# Specify alternate cities to use as the server lists are emptied.
+# These will be used in rotation.
 jdcity1="Vancouver"
 jdcity2="Seattle"
 jdcity3="Los_Angeles"
@@ -39,22 +38,52 @@ jdcity5="Atlanta"
 # Send notifications when changing servers. "y" or "n"
 notifications="y"
 #
+# Show server load in notification (may take a few seconds) "y" or "n"
+showserverload="y"
+#
 # =====================================================================
 #
 function getserverlist {
+    # https://sleeplessbeastie.eu/2019/02/18/how-to-use-public-nordvpn-api/
     curl --silent "https://api.nordvpn.com/v1/servers/recommendations" | jq --raw-output 'limit(20;.[]) | "\(.hostname)"' > "$jdlist"
     echo "EOF" >> "$jdlist"
 }
-function currentinfo {
+function getcurrentinfo {
     currenthost=$( nordvpn status | grep -i "Current server" | cut -f3 -d' ' )
     currentcity=$( nordvpn status | grep -i "City" | cut -f2 -d':' | cut -c 2- | tr ' ' '_' )
+    shorthost=$( echo "$currenthost" | cut -f1 -d'.' )
+    servercount=$(( $(wc -l < "$jdlist") - 1 ))
 }
+function getnextcity {
+    if [[ "$currentcity" == "$jdcity1" ]]; then
+        nextcity="$jdcity2"
+    elif [[ "$currentcity" == "$jdcity2" ]]; then
+        nextcity="$jdcity3"
+    elif [[ "$currentcity" == "$jdcity3" ]]; then
+        nextcity="$jdcity4"
+    elif [[ "$currentcity" == "$jdcity4" ]]; then
+        nextcity="$jdcity5"
+    else
+        nextcity="$jdcity1"
+    fi
+}
+function getserverload {
+    if [[ "$showserverload" =~ ^[Yy]$ ]]; then
+        sload=$(timeout 10 curl --silent https://api.nordvpn.com/server/stats/$currenthost | jq .percent)
+        if [[ -n "$sload" ]]; then
+            serverload=" ($sload% load)"
+        else
+            serverload=""
+        fi
+    fi
+}
+#
 # create the list if it doesn't exist
 if [[ ! -e "$jdlist" ]]; then
     getserverlist
 fi
 #
-currentinfo
+getcurrentinfo
 #
 # check if the current hostname is in the list
 if grep "$currenthost" "$jdlist"; then
@@ -68,27 +97,19 @@ nextserver=$( cat "$jdlist" | head -n 1 | cut -f1 -d'.' )
 #
 if [[ "$nextserver" == "EOF" ]]; then
     # if there are no more servers then change city and get a new list
-    if [[ "$currentcity" == "$jdcity1" ]]; then
-        nordvpn connect "$jdcity2"; wait
-    elif [[ "$currentcity" == "$jdcity2" ]]; then
-        nordvpn connect "$jdcity3"; wait
-    elif [[ "$currentcity" == "$jdcity3" ]]; then
-        nordvpn connect "$jdcity4"; wait
-    elif [[ "$currentcity" == "$jdcity4" ]]; then
-        nordvpn connect "$jdcity5"; wait
-    else
-        nordvpn connect "$jdcity1"; wait
-    fi
+    getnextcity
+    nordvpn connect "$nextcity"; wait
     sleep 3
     getserverlist
 else
     # connect to the next server
     nordvpn connect "$nextserver"; wait
+    sleep 3
 fi
 #
 if [[ "$notifications" =~ ^[Yy]$ ]]; then
-    currentinfo
-    shorthost=$( echo "$currenthost" | cut -f1 -d'.' )
-    servercount=$(( $(wc -l < "$jdlist") - 1 ))
-    notify-send "JD Reconnect - $currentcity $shorthost" "$servercount servers remaining"
+    getcurrentinfo
+    getnextcity
+    getserverload
+    notify-send "JDReconnect - $currentcity $shorthost $serverload" "$servercount servers remain.  Next city: $nextcity"
 fi
