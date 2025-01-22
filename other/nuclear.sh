71a80431ae365cb40ba859f18bace9b1c2f9ce4d
ph202107 <85448312+ph202107@users.noreply.github.com>
2025-01-22 12:16:04 -0700

    Update nuclear.sh
    Nord 3.20.0 update
    3.17.0 is now the oldest version in the repo.

--- other/nuclear.sh
+++ other/nuclear.sh
@@ -9,23 +9,6 @@
 # List available versions with: "apt list -a nordvpn"
 #
 nord_version="nordvpn"              # install the latest version available
-#nord_version="nordvpn=3.14.2"      # 29 Jul 2022 Works on Ubuntu 18.04, Mint 19.3
-#nord_version="nordvpn=3.15.0"      # 18 Oct 2022 Added login token, routing, fwmark, analytics.  Incompatible with Ubuntu 18.04, Mint 19.3
-#nord_version="nordvpn=3.15.1"      # 28 Nov 2022 Fix for older distros. Changes to "nordvpn status".  Last version to work with Ubuntu 18.04, Mint 19.3
-#nord_version="nordvpn=3.15.2"      # 06 Dec 2022 Fix for meshnet unavailable
-#nord_version="nordvpn=3.15.3"      # 29 Dec 2022 Fix for crash after suspend
-#nord_version="nordvpn=3.15.4"      # 26 Jan 2023 Faster meshnet connections
-#nord_version="nordvpn=3.15.5"      # 20 Feb 2023 Fix for Meshnet connectivity issue
-#nord_version="nordvpn=3.16.0"      # 13 Mar 2023 Additional Meshnet features
-#nord_version="nordvpn=3.16.1"      # 28 Mar 2023 Fix for OpenVPN on Fedora
-#nord_version="nordvpn=3.16.2"      # 26 Apr 2023 Legacy logins removed. Meshnet notifications
-#nord_version="nordvpn=3.16.3"      # 01 Jun 2023 OpenVPN security upgrade.
-#nord_version="nordvpn=3.16.4"      # 31 Jul 2023 Fix Port/subnet whitelist. Meshnet transfers
-#nord_version="nordvpn=3.16.5"      # 02 Aug 2023 Fix for GOCOVERDIR error in 3.16.4
-#nord_version="nordvpn=3.16.6"      # 18 Sep 2023 LAN Discovery added. Whitelist is now Allowlist
-#nord_version="nordvpn=3.16.7"      # 31 Oct 2023 Improved help, fixed Meshnet high CPU
-#nord_version="nordvpn=3.16.8"      # 14 Nov 2023 Improved Meshnet speeds
-#nord_version="nordvpn=3.16.9"      # 06 Dec 2023 Minor tweaks and fixes
 #nord_version="nordvpn=3.17.0"      # 16 Jan 2024 Meshnet peer rename, clear transfer history. DNS leak issue: https://github.com/NordSecurity/nordvpn-linux/issues/243
 #nord_version="nordvpn=3.17.1"      # 12 Feb 2024 /etc/resolv.conf DNS fix, bug fixes. IPv6 issue: https://github.com/NordSecurity/nordvpn-linux/issues/243
 #nord_version="nordvpn=3.17.2"      # 14 Feb 2024 bug fix for IPv6 issue in 3.17.1
@@ -38,7 +21,9 @@ nord_version="nordvpn"              # install the latest version available
 #nord_version="nordvpn=3.18.4"      # 19 Aug 2024 Bug fixes - routing, tray, dedicated-ip, gateway, allowlist.
 #nord_version="nordvpn=3.18.5"      # 05 Sep 2024 Internal changes.
 #nord_version="nordvpn=3.19.0"      # 30 Sep 2024 Post-Quantum VPN added.
-#nord_version="nordvpn=3.19.1"      # 19 Nov 2024 Bugfixes. Meshnet fileshare library changed.
+#nord_version="nordvpn=3.19.1"      # 18 Nov 2024 Bugfixes. Meshnet fileshare library changed.
+#nord_version="nordvpn=3.19.2"      # 27 Nov 2024 OpenVPN bugfix.
+#nord_version="nordvpn=3.20.0"      # 21 Jan 2025 Significant security improvements. NordWhisper technology added.
 #
 # v3.15.0+ can login using a token. Leave blank for earlier versions.
 # To create a token visit https://my.nordaccount.com/ - Services - NordVPN - Manual Setup - Generate New Token
